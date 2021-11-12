fbd7d2b34008ee277ac7fe1e089dc00bd000a2c1
neusj47 <neusj47@gmail.com>
2021-11-13 01:41:32 +0900

    # commit

--- DMDB_Updater(Q2).py
+++ DMDB_Updater(Q2).py
@@ -7,6 +7,7 @@
 import dart_fss as dart
 import pandas as pd
 import requests
+from datetime import datetime
 
 file_path = "C:/Users/ysj/PycharmProjects/database/dmfs/"
 api_key= ''
@@ -72,12 +73,10 @@ def download_fin_stats(start_date, ticker, period):
     fs.save(ticker + '.xlsx', path = file_path)
     return
 
-code = ticker.code[1]
-
 # 2. 재무제표 가공하기
 def get_bstats(fs) :
     b = fs.show('bs')
-    entrance = '[D210000] Statement of financial position, current/non-current - Consolidated financial statements (Unit: KRW)'
+    entrance = b.columns[0][0]
     b_cols = ['ifrs-full_CurrentAssets','ifrs-full_Inventories','ifrs-full_Assets','ifrs-full_CurrentLiabilities',
             'ifrs-full_Liabilities', 'ifrs-full_RetainedEarnings', 'ifrs-full_Equity']
     b_stats = b[b[entrance].concept_id.isin(b_cols)].reset_index(drop=True)
@@ -88,7 +87,7 @@ def get_bstats(fs) :
         if b_cols[i] not in b_list :
             df_b[b_cols[i]] = 0
         else :
-            df_b[b_cols[i]] = b_stats[b_stats[entrance].concept_id == b_cols[i]].iloc[0,7:len(b_stats.columns)].reset_index()[i]
+            df_b[b_cols[i]] = b_stats[b_stats[entrance].concept_id == b_cols[i]].iloc[0,7:len(b_stats.columns)].reset_index().iloc[:,2]
     df_b = df_b.dropna(axis = 0)
     df_b = df_b.rename(
         columns={'level_0': 'last_update', 'ifrs-full_CurrentAssets': 'CA', 'ifrs-full_Inventories': 'INV',
@@ -99,7 +98,7 @@ def get_bstats(fs) :
 
 def get_istats(fs) :
     i = fs.show('cis')
-    entrance = '[D431410] Statement of comprehensive income, by function of expense - Consolidated financial statements (Unit: KRW)'
+    entrance = i.columns[0][0]
     i_cols = ['ifrs-full_Revenue', 'ifrs-full_GrossProfit','dart_OperatingIncomeLoss','ifrs-full_ProfitLoss',
               'ifrs-full_ComprehensiveIncome', 'ifrs-full_BasicEarningsLossPerShare']
     i_stats = i[i[entrance].concept_id.isin(i_cols)].reset_index(drop=True)
@@ -110,13 +109,24 @@ def get_istats(fs) :
         if i_cols[i] not in i_list :
             df_i[i_cols[i]] = 0
         else :
-            df_i[i_cols[i]]  = i_stats[i_stats[entrance].concept_id == i_cols[i]].iloc[0,7:len(i_stats.columns)].reset_index()[i]
+            df_i[i_cols[i]]  = i_stats[i_stats[entrance].concept_id == i_cols[i]].iloc[0,7:len(i_stats.columns)].reset_index().iloc[:,2]
     df_i = df_i.dropna(axis = 0)
     df_i = df_i.rename(
         columns={'ifrs-full_Revenue': 'REV', 'ifrs-full_GrossProfit': 'GP', 'dart_OperatingIncomeLoss': 'OI',
                  'ifrs-full_ProfitLoss': 'NI', 'ifrs-full_ComprehensiveIncome': 'CI',
                  'ifrs-full_BasicEarningsLossPerShare': 'EPS'
                  })
+    df_i['last_update'] = datetime.today()
+    for i in range(0, len(df_i)):
+        if int(df_i.level_0[i][9:]) - int(df_i.level_0[i][:8]) > 300:
+            df_i = df_i.drop(index=i)
+    df_i = df_i.reset_index(drop=True)
+    for i in range(0, len(df_i)):
+        df_i['last_update'][i] = datetime.strptime(df_i.level_0[1][9:], '%Y%m%d').strftime('%Y%m%d')
+    df_i = df_i[['last_update', 'REV', 'GP', 'OI', 'NI', 'CI', 'EPS']]
+    df_i['GPR'] = df_i['GP'] / df_i['REV']
+    df_i['OIR'] = df_i['OI'] / df_i['REV']
+    df_i['NIR'] = df_i['NI'] / df_i['REV']
     return df_i
 
 def get_cstats(fs) :
@@ -125,7 +135,7 @@ def get_cstats(fs) :
               'ifrs-full_ProceedsFromSalesOfPropertyPlantAndEquipmentClassifiedAsInvestingActivities',
               'ifrs-full_PurchaseOfPropertyPlantAndEquipmentClassifiedAsInvestingActivities',
               'ifrs-full_CashFlowsFromUsedInFinancingActivities']
-    entrance = '[D520000] Statement of cash flows, indirect method - Consolidated financial statements (Unit: KRW)'
+    entrance = c.columns[0][0]
     c_stats = c[c[entrance].concept_id.isin(c_cols)].reset_index(drop=True)
     c_list = c_stats[entrance].concept_id.to_list()
     temp = c_stats.iloc[0, 7:len(c_stats.columns)].reset_index()
@@ -134,42 +144,36 @@ def get_cstats(fs) :
         if c_cols[i] not in c_list :
             df_c[c_cols[i]] = 0
         else :
-            temp = c_stats[c_stats[entrance].concept_id == c_cols[i]].iloc[0,7:len(c_stats.columns)].reset_index()
-            df_c[c_cols[i]] = temp[i]
+            df_c[c_cols[i]] = c_stats[c_stats[entrance].concept_id == c_cols[i]].iloc[0,7:len(c_stats.columns)].reset_index().iloc[:,2]
     df_c = df_c.dropna(axis = 0)
     df_c = df_c.rename(columns={'ifrs-full_CashFlowsFromUsedInOperatingActivities': 'CFO',
                                 'ifrs-full_CashFlowsFromUsedInInvestingActivities': 'CFI',
                                 'ifrs-full_ProceedsFromSalesOfPropertyPlantAndEquipmentClassifiedAsInvestingActivities': 'CPX_IN',
                                 'ifrs-full_PurchaseOfPropertyPlantAndEquipmentClassifiedAsInvestingActivities': 'CPX_OUT',
                                 'ifrs-full_CashFlowsFromUsedInFinancingActivities': 'CFF'})
-    return df_c
+    df_c['last_update'] = datetime.today()
+    for i in range(0, len(df_c)):
+        if int(df_c.level_0[i][9:]) - int(df_c.level_0[i][:8]) > 300:
+            df_c = df_c.drop(index=i)
+    df_c = df_c.reset_index(drop=True)
+    for i in range(0, len(df_c)):
+        df_c['last_update'][i] = datetime.strptime(df_c.level_0[1][9:], '%Y%m%d').strftime('%Y%m%d')
+    df_c = df_c[['last_update', 'CFO', 'CFI', 'CPX_IN', 'CPX_OUT', 'CFF']]
+    df_c['FCF'] = df_c['CFO'] + df_c['CPX_IN'] - df_c['CPX_OUT']
 
+    return df_c
 # 3. DB 입력하기
 for i, code in enumerate(ticker.code) :
     try :
+
         fs = dart.get_corp_list().find_by_stock_code(code).extract_fs(bgn_de=start_date, report_tp=[period])
+
         df_b = get_bstats(fs)
 
         df_i = get_istats(fs)
-        df_i['last_update'] = ''
-        for i in range(0, len(df_i)):
-            if int(df_i.level_0[i][9:]) - int(df_i.level_0[i][:8]) > 300:
-                df_i = df_i.drop(index=i)
-        df_i = df_i.reset_index(drop=True)
-        for i in range(0, len(df_i)):
-            df_i['last_update'][i] = df_i.level_0[i][9:]
-        df_i = df_i[['last_update', 'REV', 'GP', 'OI', 'NI', 'CI', 'EPS']]
 
         df_c = get_istats(fs)
-        df_c['last_update'] = ''
-        for i in range(0, len(df_c)):
-            if int(df_c.level_0[i][9:]) - int(df_c.level_0[i][:8]) > 300:
-                df_c = df_c.drop(index=i)
-        df_c = df_c.reset_index(drop=True)
-        for i in range(0, len(df_c)):
-            df_c['last_update'][i] = df_c.level_0[i][9:]
-        df_c = df_c[['last_update', 'CFO', 'CFI', 'CPX_IN', 'CPX_OUT', 'CFF']]
 
-        print(i, ' 번 째 입력완료 : ', code)
+        print(i, ' 번 째 산출 완료 : ', code)
     except Exception as e:
             print(i, ' 번 째 오류 발생 : ', code, ' 오류:', str(e))
